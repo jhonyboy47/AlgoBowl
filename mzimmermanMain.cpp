0d9ad4b0b81e54e6cb0eac6a416934d45f573d95
mzimmerman5 <mzimmerman@mymail.mines.edu>
2021-03-03 09:55:20 -0700

    code finished
    this works, is probably not optimal though

--- mzimmermanMain.cpp
+++ mzimmermanMain.cpp
@@ -3,24 +3,30 @@
 #include <string>
 #include <vector>
 #include <unordered_set>
+#include <algorithm>
+#include <iterator>
+#include <sstream>
 using namespace std;
 
 //variables
 string currentLine;
-int n = 2;
+//int n = 2;
 vector<int> neededInts;
-vector<int> copyOfNeededInts = {3, 5};
-int iterations = 1;
+vector<string> neededIntsStrings;
+//vector<int> copyOfNeededInts = {3, 5};
+//int iterations = 1;
 vector<string> outputLists;
-unordered_set<string> outputList;
+//unordered_set<string> outputList;
 vector<int> numsToUse;
 vector<string> lines;
-//{3, 5, 8, 16, 20, 41, 104, 198, 348, 399, 518}{3, 5, 8, 16, 20, 41, 104, 198, 348, 399, 518}
+//{3, 5, 8, 16, 20, 41, 104, 198, 348, 399, 518}
 
 int main() {
     // file read in
     string currentLine;
-    ifstream myFile ("testInput.txt");
+    ifstream myFile;
+    myFile.open("input");
+
     if(myFile.is_open()) {
         while(getline(myFile,currentLine)) {
             lines.emplace_back(currentLine);
@@ -28,15 +34,10 @@ int main() {
         myFile.close();
     }
 
-    for(int p = 0; p < lines.at(1).length(); p++) {
-        if(lines.at(1).at(p) == ' ') {
-            neededInts.emplace_back(stoi(lines.at(1).substr(0, p)));
-            lines.at(1).erase(0, p);
-        }
-    }
-    for(int q = 0; q < neededInts.size(); q++) {
-        cout << neededInts.at(q);
-        cout << endl;
+    string tempString;
+    istringstream ss(lines.at(1));
+    while(getline(ss,tempString, ' ')) {
+        neededInts.emplace_back(stoi(tempString));
     }
 
     //actual heuristic or whatever
@@ -46,7 +47,7 @@ int main() {
     for(int k = 0; k < neededInts.size(); k++) {
         int tempInt = 0;
         bool foundNeededInt = false;
-        string tempString = "";
+        string tempString;
         for(int i = numsToUse.size() - 1; i >= 0; i--) {
             for(int j = numsToUse.size() - 1; j >= 0; j--) {
                 tempInt = numsToUse.at(i) + numsToUse.at(j);
@@ -73,9 +74,13 @@ int main() {
             }
         }
     }
-    for(int m = 0; m < outputLists.size(); m++) {
-        cout << outputLists.at(m);
-        cout << endl;
+    ofstream outputFile;
+    outputFile.open ("output.txt");
+    outputFile << to_string(outputLists.size());
+    for(int m = 1; m < outputLists.size(); m++) {
+        outputFile << "\n";
+        outputFile << outputLists.at(m);
     }
+    outputFile.close();
 }
 
